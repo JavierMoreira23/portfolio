9601b82d7169c62f14b10626c997246f0a40e327
Javier Moreira <javier10moreira@hotmail.com>
2024-12-02 20:36:12 -0500

    cambio de fondo y añadiendo fuente principal

--- tailwind.config.js
+++ tailwind.config.js
@@ -18,10 +18,18 @@ module.exports = {
 		lg: '960px',
 		xl: '1200px',
 	},
+	fontFamily: {
+		primary: "var(--font-jetbrainsMono)",
+	},
   	extend: {
+
   		colors: {
-  			background: 'var(--background)',
-  			foreground: 'var(--foreground)'
+			primary: '#1c1c22',
+			accent:{
+				DEFAULT: '#00ff99',
+				hover: "#00e187",
+			},
+  			
   		},
   		borderRadius: {
   			lg: 'var(--radius)',
